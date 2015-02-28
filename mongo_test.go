bbb2b29c91c2f9d07349fb30dca53f96dcd82f9a
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-02-28 18:13:19 +0700

    add get all contacts

--- mongo_test.go
+++ mongo_test.go
@@ -2,6 +2,7 @@ package contact
 
 import (
 	"os"
+	"reflect"
 	"testing"
 
 	"gopkg.in/mgo.v2"
@@ -43,5 +44,27 @@ func TestGetID(t *testing.T) {
 	}
 	if info.Content != "your content" {
 		t.Error("expect content: your content")
+	}
+}
+
+func TestGetAllContact(t *testing.T) {
+	c := s.DB("test").C("contact")
+	defer c.DropCollection()
+
+	var expectInfos = []Information{
+		{Email: "mail@email.com", Title: "Information", Content: "your content"},
+		{Email: "mail1@email.com", Title: "Information1", Content: "your content"},
+		{Email: "mail2@email.com", Title: "Information2", Content: "your content"},
+	}
+	c.Insert(expectInfos[0])
+	c.Insert(expectInfos[1])
+	c.Insert(expectInfos[2])
+
+	mp := &MongoProvider{session: s}
+
+	var infos []Information = mp.All()
+
+	if !reflect.DeepEqual(infos, expectInfos) {
+		t.Error("expect infomations is", expectInfos, "but is", infos)
 	}
 }
